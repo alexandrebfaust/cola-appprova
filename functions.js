f067cda80b5032036f767540c61ecb8e98272087
Alexandre B Faust <alexandrebfaust@gmail.com>
2017-10-04 12:25:56 -0300

    Add files via upload
    Update preventivo
    Você pode acompanhar a atualização do seu aplicativo diretamente pelo ícone no navegador

--- functions.js
+++ functions.js
@@ -1,6 +1,10 @@
 if(window.location.href.indexOf("appprova") > -1) {
 
-   document.getElementsByClassName("trail-info__item")[0].innerHTML+= "<div style='color:#000;display:scroll;position:fixed;bottom:0px;right:1px;'><button id='on' style='background-color:#0f0;'>On</button><button id='off' style='background-color:#f00;'>Off</button></div>";
+var version = "4";
+var url = document.URL;
+
+   document.getElementsByClassName("trail-info__item")[0].innerHTML+= "<div style='color:#000;display:scroll;position:fixed;bottom:0px;right:1px;'><button id='on' style='background-color:#0f0;'>On</button><button id='off' style='background-color:#f00;'>Off</button></div>"; 
+   document.getElementsByClassName("trail-info__item")[0].innerHTML+= "<a target='_blank' href='https://github.com/alexandrebfaust/cola-appprova'><img src='http://appprova.projeto.cf/verifica.php?v="+version+"&url="+url+"'></a>";
 
    $(document).ready(function(){
 	   $('#on').click( function() {
--- popup.js
+++ popup.js
@@ -1,1 +1,4 @@
-/* Att */
+/* Verifica atualização */
+var version = "4";
+var url = document.URL;
+document.getElementById('atualizacao').innerHTML = "<a target='_blank' href='https://github.com/alexandrebfaust/cola-appprova'><img src='http://appprova.projeto.cf/atualizacao.php?v="+version+"&url="+url+"'></a>";
