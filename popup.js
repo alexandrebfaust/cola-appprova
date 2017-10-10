a6c782eed32e461bba4b8c53940c57e62e65b893
Alexandre B Faust <alexandrebfaust@gmail.com>
2017-10-10 14:47:37 -0300

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
 /* Verifica atualização */
-var version = "4";
+var version = "5";
 var url = document.URL;
 document.getElementById('atualizacao').innerHTML = "<a target='_blank' href='https://github.com/alexandrebfaust/cola-appprova'><img src='http://appprova.projeto.cf/atualizacao.php?v="+version+"&url="+url+"'></a>";
