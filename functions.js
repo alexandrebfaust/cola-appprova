0311de8e6e87eb92bd38638e3f5a515d64fae2c8
Alexandre B Faust <alexandrebfaust@gmail.com>
2017-10-10 14:47:50 -0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,6 +1,6 @@
 if(window.location.href.indexOf("appprova") > -1) {
 
-var version = "4";
+var version = "5";
 var url = document.URL;
 
    document.getElementsByClassName("trail-info__item")[0].innerHTML+= "<div style='color:#000;display:scroll;position:fixed;bottom:0px;right:1px;'><button id='on' style='background-color:#0f0;'>On</button><button id='off' style='background-color:#f00;'>Off</button></div>"; 
